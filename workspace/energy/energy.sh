6f4dd7a78d5f67d558458e638868b713737d2171
Sihua Li <sihua@maverick-1.Stanford.EDU>
2015-11-02 00:33:13 -0800

    energy

--- workspace/energy/energy.sh
+++ workspace/energy/energy.sh
@@ -2,37 +2,37 @@
 
 set -e
 
-debug() { echo "D> $*" 1>&2; }
-
 MAIN_PATH=~/cs316/zsim_build/downloads/zsim/workspace/energy/src
 
-## default
-CORES_b=6
-CORES_w=10
-CORES=16
-FREQUENCY=2400
-L1SIZEb=32
-L1WAYSb=4
-L1SIZEw=8
-L1WAYSw=4
-L2SIZE=256
-L2WAYS=8
-L3SIZE=8192
-L3WAYS=16
-L3REPL="LRU"
-MEMRANKS=2
-MEMTECH="DDR3-1066-CL8"
+## configuration parameters
+TIME=`cut -f1 -d " " args.txt`
+FREQUENCY=`cut -f2 -d " " args.txt`
+CORES_b=`cut -f3 -d " " args.txt`
+CORES_w=`cut -f4 -d " " args.txt`
+L1SIZEb=`cut -f5 -d " " args.txt`
+L1WAYSb=`cut -f6 -d " " args.txt`
+L1SIZEw=`cut -f7 -d " " args.txt`
+L1WAYSw=`cut -f8 -d " " args.txt`
+L2SIZE=`cut -f9 -d " " args.txt`
+L2WAYS=`cut -f10 -d " " args.txt`
+L3SIZE=`cut -f11 -d " " args.txt`
+L3WAYS=`cut -f12 -d " " args.txt`
+DRAM_TECH=`cut -f13 -d " " args.txt`
+CORE_DYN_ENERGY=`cut -f14 -d " " args.txt`
+CORE_STA_ENERGY=`cut -f15 -d " " args.txt`
+
+CORES=`echo $CORES_b+$CORES_w | bc -l`
 
 ## Run CACTI 6.5 to get cache parameters
-L1_CACTI_beefy=`sh $MAIN_PATH/gen_cacti.sh -s $L1SIZEb -w $L1WAYSb`
-L1_CACTI_wimpy=`sh $MAIN_PATH/gen_cacti.sh -s $L1SIZEw -w $L1WAYSw`
-L2_CACTI=`sh $MAIN_PATH/gen_cacti.sh -s $L2SIZE -w $L2WAYS`
-L3_CACTI=`sh $MAIN_PATH/gen_cacti.sh -s $L3SIZE -w $L3WAYS`
-# <size> <way> <type> <cycle time> <access time> <energy> <power> <area>
-echo $L1_CACTI_beefy > cacti_L1_beefy.txt
-echo $L1_CACTI_wimpy > cacti_L1_wimpy.txt
-echo $L2_CACTI > cacti_L2.txt
-echo $L3_CACTI > cacti_L3.txt
+L1_CACTI_beefy=`$MAIN_PATH/gen_cacti.sh -s $L1SIZEb -w $L1WAYSb`
+L1_CACTI_wimpy=`$MAIN_PATH/gen_cacti.sh -s $L1SIZEw -w $L1WAYSw`
+L2_CACTI=`$MAIN_PATH/gen_cacti.sh -s $L2SIZE -w $L2WAYS`
+L3_CACTI=`$MAIN_PATH/gen_cacti.sh -s $L3SIZE -w $L3WAYS`
+
+#echo $L1_CACTI_beefy > cacti_L1_beefy.txt
+#echo $L1_CACTI_wimpy > cacti_L1_wimpy.txt
+#echo $L2_CACTI > cacti_L2.txt
+#echo $L3_CACTI > cacti_L3.txt
 
 L1_ENERGY_b=`cut -f6 -d " " cacti_L1_beefy.txt`
 L1_POWER_b=`cut -f7 -d " " cacti_L1_beefy.txt`
@@ -45,14 +45,13 @@ L3_ENERGY=`cut -f6 -d " " cacti_L3.txt`
 L3_POWER=`cut -f7 -d " " cacti_L3.txt`
 L3_AREA=`cut -f8 -d " " cacti_L3.txt`
 
-#TIME=`python /afs/ir/users/s/i/sihua/cs316/zsim_backup/zsim_build/extract.py`
-
+# Beefy L1 cache energy
 L1_hGETS=`grep hGETS zsim.out | head -$[2*CORES_b] | awk '{sum += $2} END {print sum}'`
 L1_hGETX=`grep hGETX zsim.out | head -$[2*CORES_b] | awk '{sum += $2} END {print sum}'`
 L1_mGETS=`grep mGETS zsim.out | head -$[2*CORES_b] | awk '{sum += $2} END {print sum}'`
 
-L1_DYN_ENERGY=`echo "($L1_hGETS+$L1_hGETX+$L1_mGETS)*$L1_ENERGY/1000000000" | bc -l`
-#L1_STA_ENERGY=`echo "$CORES_b*$L1_POWER/1000*$TIME" | bc -l`
+L1_DYN_ENERGY=`echo "($L1_hGETS+$L1_hGETX+$L1_mGETS)*$L1_ENERGY_b/1000000000" | bc -l`
+L1_STA_ENERGY=`echo "$CORES_b*$L1_POWER/1000*$TIME" | bc -l`
 
 tmp_L2_hGETS=`grep hGETS zsim.out | head -$[3*CORES_b] | awk '{sum += $2} END {print sum}'`
 L2_hGETS=$[tmp_L2_hGETS - L1_hGETS]
@@ -62,17 +61,41 @@ tmp_L2_mGETS=`grep mGETS zsim.out | head -$[3*CORES_b] | awk '{sum += $2} END {p
 L2_mGETS=$[tmp_L2_mGETS - L1_mGETS]
 
 L2_DYN_ENERGY=`echo "($L2_hGETS+$L2_hGETX+$L2_mGETS)*$L2_ENERGY/1000000000" | bc -l`
-#L2_STA_ENERGY=`echo "$CORES_B*$L2_POWER/1000*$TIME" | bc -l`
+L2_STA_ENERGY=`echo "$CORES_b*$L2_POWER/1000*$TIME" | bc -l`
 
 L3_hGETS=`grep hGETS zsim.out | tail -1 | awk '{print $2}'`
 L3_hGETX=`grep hGETX zsim.out | tail -1 | awk '{print $2}'`
+L3_mGETS=`grep mGETS zsim.out | tail -1 | awk '{print $2}'`
 
+L3_DYN_ENERGY=`echo "($L3_hGETS+$L3_hGETX+$L3_mGETS)*$L3_ENERGY/1000000000" | bc -l`
+L3_STA_ENERGY=`echo "$L3_POWER/1000*$TIME" | bc -l`
+
+MEM_RD=`grep -w rd zsim.out | awk '{print $2}' | paste -sd+ | bc`
+MEM_WR=`grep -w wr zsim.out | awk '{print $2}' | paste -sd+ | bc`
+
+#echo "python /afs/ir/class/ee282/pa2/bin/mem.py $DRAM_TECH $MEM_RD $MEM_WR $TIME"
+MEM_DYN_ENERGY=`python /afs/ir/class/ee282/pa1/bin/mem.py $DRAM_TECH $MEM_RD $MEM_WR $TIME false`
+MEM_STA_ENERGY=`python /afs/ir/class/ee282/pa1/bin/mem.py $DRAM_TECH $MEM_RD $MEM_WR $TIME true`
+
+TOTAL_ENERGY=`echo $CORE_DYN_ENERGY+$CORE_STA_ENERGY+$L1_DYN_ENERGY+$L1_STA_ENERGY+$L2_DYN_ENERGY+$L2_STA_ENERGY+$L3_DYN_ENERGY+$L3_STA_ENERGY+$MEM_DYN_ENERGY+$MEM_STA_ENERGY | bc -l`
+EDP=`echo $TOTAL_ENERGY*$TIME | bc -l`
+
+echo "Execution    time (s)  : $TIME"
 echo "L1 dynamic energy (J)  : $L1_DYN_ENERGY"
 echo "L1 static energy (J)   : $L1_STA_ENERGY"
 echo "L2 dynamic energy (J)  : $L2_DYN_ENERGY"
 echo "L2 static energy (J)   : $L2_STA_ENERGY"
 echo "L3 dynamic energy (J)  : $L3_DYN_ENERGY"
 echo "L3 static energy (J)   : $L3_STA_ENERGY"
-echo "$L1_hGETS"
+echo "Mem dynamic energy (J) : $MEM_DYN_ENERGY"
+echo "Mem static energy (J)  : $MEM_STA_ENERGY"
 
+echo "$L1_ENERGY_b"
+echo "$L1_POWER_b"
+echo "$L2_ENERGY"
+echo "$L2_POWER"
+echo "$L2_AREA"
+echo "$L3_ENERGY"
+echo "$L3_POWER"
+echo "$L3_AREA"
 
--- workspace/energy/extract.py
+++ workspace/energy/extract.py
@@ -10,7 +10,7 @@ ZSIMH5_PATH = '../run/zsim-ev.h5'
 
 cfg = ps.parse(CFG_PATH)
 
-pprint.pprint(cfg)
+#pprint.pprint(cfg)
 
 frequency = float(cfg['sys']['frequency'])
 cores_beefy = int(cfg['sys']['cores']['beefy']['cores'])
@@ -30,60 +30,39 @@ L3SIZE = int(cfg['sys']['caches']['l3']['size'])/1024
 L3WAYS = int(cfg['sys']['caches']['l3']['size'])
 MEM_TECH = cfg['sys']['mem']['tech']
 
-print frequency, cores_beefy, cores_wimpy, phaseLength, L1DSIZE_beefy, L1DWAYS_beefy, L2SIZE, L2WAYS, MEM_TECH 
+#print frequency, cores_beefy, cores_wimpy, phaseLength, L1DSIZE_beefy, L1DWAYS_beefy, L2SIZE, L2WAYS, MEM_TECH 
 
 # Open stats file
 f = h5py.File(ZSIMH5_PATH, 'r')
 
 # Get the single dataset in the file
 dset = f["stats"]["root"]
-print dset.shape
-print dset.dtype
+#print dset.shape
+#print dset.dtype
 
 # Phase count at end of simulation
 endPhase = dset[-1]['phase']
 ccycles = endPhase * phaseLength
 TIME = float(ccycles / frequency) * 0.000001
-print 'Cores:', cores_beefy + cores_wimpy
-print 'Frequency (MHz):', frequency
-print 'Execution Time (s):', TIME
 
 # Core Voltage
 CORE_VOLTAGE_B = (frequency/1000) * .30914 + .552688
 CORE_VOLTAGE_W = (frequency/1000) * .21967 + .66066 
-print 'Beefy Core Voltage (V):', CORE_VOLTAGE_B
-print 'Wimpy Core Voltage (V):', CORE_VOLTAGE_W
-
-# If your L2 has a single bank, this is all the L2 hits. Otherwise it's the
-# hits of the first L2 bank
-#l2_hits = dset[-1]['l2_beefy'][0]['hGETS'] + dset[-1]['l2_beefy'][0]['hGETX']
-#print 'l2 hits:', l2_hits
 
 # Total number of instructions executed, counted by adding per-core counts
 instrs1 = np.sum(dset[-1]['beefy']['instrs']) 
 instrs2 = np.sum(dset[-1]['wimpy']['instrs'])
 instructions = instrs1 + instrs2;
 
-# Calculate energy
+# Calculate core energy
 I_leak_beefy=1.3136495
 I_leak_wimpy=0.45455
 CORE_DYN_ENERGY_B= 2.22/1000000000*instrs1*(CORE_VOLTAGE_B/1.35)*(CORE_VOLTAGE_B/1.35)
 CORE_STA_ENERGY_B= I_leak_beefy*CORE_VOLTAGE_B*TIME*cores_beefy
-print 'beefy core dynamic energy (W):',CORE_DYN_ENERGY_B
-print 'beefy core static energy (W):',CORE_STA_ENERGY_B
 CORE_DYN_ENERGY_W= .78/1000000000*instrs2*(CORE_VOLTAGE_W/1.16)*(CORE_VOLTAGE_W/1.16)
 CORE_STA_ENERGY_W= I_leak_wimpy*CORE_VOLTAGE_W*TIME*cores_wimpy
-print 'wimpy core dynamic energy (W):',CORE_DYN_ENERGY_W
-print 'wimpy core static energy (W):',CORE_STA_ENERGY_W
-print 'Core dynamic energy (J):', CORE_DYN_ENERGY_B + CORE_DYN_ENERGY_W
-print 'Core static energy (J):', CORE_STA_ENERGY_B + CORE_STA_ENERGY_W
+CORE_DYN_ENERGY=CORE_DYN_ENERGY_B+CORE_DYN_ENERGY_W
+CORE_STA_ENERGY=CORE_STA_ENERGY_B+CORE_STA_ENERGY_W
 
-l1_hgets_b = np.sum(dset[-1]['l1i_beefy']['hGETS']) + np.sum(dset[-1]['l1d_beefy']['hGETS'])
-l1_hgetx_b = np.sum(dset[-1]['l1i_beefy']['hGETX']) + np.sum(dset[-1]['l1d_beefy']['hGETX'])
-l1_miss_b = np.sum(dset[-1]['l1i_beefy']['mGETS']) + np.sum(dset[-1]['l1d_beefy']['mGETS'])
-
-l2_hgets_b = np.sum(dset[-1]['l2_beefy']['hGETS'])
-l2_hgetx_b = np.sum(dset[-1]['l2_beefy']['hGETX'])
-l2_miss_b = np.sum(dset[-1]['l2_beefy']['mGETS'])
-
-print l1_hgets_b
+f = open('args.txt', 'w')
+f.write(str(TIME) + ' ' + str(frequency) + ' ' + str(cores_beefy) + ' ' + str(cores_wimpy) + ' ' + str(L1ISIZE_beefy) + ' ' + str(L1IWAYS_beefy) + ' ' + str(L1ISIZE_wimpy) + ' ' + str(L1IWAYS_wimpy) + ' ' + str(L2SIZE) + ' ' + str(L2WAYS) + ' ' + str(L3SIZE) + ' ' + str(L3WAYS) + ' ' + str(MEM_TECH) + ' ' + str(CORE_DYN_ENERGY) + ' ' + str(CORE_STA_ENERGY))
