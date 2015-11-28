54eba459430999b0f035fa1098705eead5eda1e7
infinitespace <vincent.zhang.us@gmail.com>
2015-11-28 13:39:31 -0800

    start adding heuristic algorithm

---
+++ workspace/algorithm/power-efficient-heuristic.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+import sys
+import argparse
+import random
+import copy
+
+def loadMap(mapdir):
+    newmap = [[]]
+    try:
+        newmap = pickle.load(open(mapdir, 'r'))
+    except:
+        print "!!!ERROR!!!: can not open and load input file from: ", mapdir
+    return newmap
+
+def runPEH(tmapdir, pmapdir, cmapdir, outputfile):
+    # tmap = loadMap(tmapdir)
+    # pmap = loadMap(pmapdir)
+    tmap = getRandomMap(8, 4)
+    pmap = getRandomMap(8, 4)
+    cmap = [0,0,1,1,2,2,3,3]
+    pnum = len(tmap) # process number
+    n = len(cmap)    # total core number
+    m = 4
+    M = copy.deepcopy(tmap)
+    X = np.zeros([pnum, n])
+    print tmap, pmap, cmap, n, m, M, X
+
+    m_to_one = range(1, m+1)
+    m_to_one.reverse()
+    
+    # Core types are ordered from smallest to biggest and assigned indices from 1 to m
+    for j in m_to_one:
+        
+    print m_to_one
+
+def getRandomMap(height, width):
+    m = []
+    for i in range(height):
+        row = []
+        row.append(random.random() * 10)
+        m.append(row)
+    return m
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = "Run power-efficient-heuristic algorithm to generate a map.dat")
+    parser.add_argument('-t', help = 'throughput map dir', required = False)
+    parser.add_argument('-p', help = 'power map dir', required = False)
+    parser.add_argument('-c', help = 'core map dir', required = False)
+    parser.add_argument('-o', help = 'output map dir', required = False)
+    args = parser.parse_args()
+
+    tmapdir = 'tmap.pkl'
+    pmapdir = 'pmap.pkl'
+    cmapdir = 'cmap.pkl'
+    outputfile = 'map.dat'
+    if args.t:
+        tmapdir = args.t
+    if args.p:
+        pmapdir = args.p
+    if args.c:
+        pmapdir = args.c
+    if args.o:
+        outputfile = args.o
+    runPEH(tmapdir, pmapdir, cmapdir, outputfile)
